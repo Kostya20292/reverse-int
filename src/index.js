2f50fdcaad36d3c43c91a2dc6941af897c0b4eaa
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2022-01-09 21:22:35 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString();
+    let newStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return +newStr;
+};
