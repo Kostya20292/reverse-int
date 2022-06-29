bd1bd9bd459fb08c55bae09436fe8843a972edd2
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2022-06-29 13:39:20 +0300

    Complite task

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 module.exports = function reverse(n) {
-    let str = Math.abs(n).toString();
-    let newStr = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        newStr += str[i];
-    }
-    return +newStr;
+    const string = Math.abs(n).toString();
+    const array = +string.split("").reverse().join("");
+    return array;
 };
